571bf5c58fc9f229c17b65eb04b77f7d58739aa3
nafnoe <noeliaarodas@gmail.com>
2025-09-10 00:33:31 -0300

    includes-faltantes

---
+++ altas/includes/id-cuentas.cbl
@@ -0,0 +1,7 @@
+       01 ID-CUENTA.
+             05 ID-D1   PIC 9.
+             05 ID-D2   PIC 9.
+             05 ID-D3   PIC 9.
+             05 ID-D4   PIC 9.
+             05 ID-D5   PIC 9.
+             
